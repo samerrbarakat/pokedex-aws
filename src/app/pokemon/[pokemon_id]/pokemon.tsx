c9b2e379931e04c15edf4a8dfcdf4b31edc622bc
samerrbarakat <barakatsamer932@gmail.com>
2025-05-27 19:16:00 +0300

    first attempt at pokemon properties

--- src/app/pokemon/[pokemon_id]/pokemon.tsx
+++ src/app/pokemon/[pokemon_id]/pokemon.tsx
@@ -1,5 +1,14 @@
 import Pokemon from "@/model/pokemon";
-import { Row, Col, Container, Image } from "react-bootstrap";
+import {
+  Row,
+  Col,
+  Container,
+  Image,
+  ProgressBar,
+  Card,
+  ListGroup,
+  Badge,
+} from "react-bootstrap";
 
 type Props = {
   pokemon: Pokemon;
@@ -7,6 +16,19 @@ type Props = {
 
 export default function PokemonComponent(props: Props) {
   const { pokemon } = props;
+
+  const getFamilyTag = (pokemon: Pokemon, evolution: string) => {
+    if (evolution === pokemon.devolution) {
+      return <Badge bg="danger">Devolution</Badge>;
+    }
+    if (evolution === pokemon.pokemonName) {
+      return <Badge bg="primary">Current</Badge>;
+    }
+    if (evolution === pokemon.evolution) {
+      return <Badge bg="success">Evolution</Badge>;
+    }
+    return <></>;
+  };
 
   return (
     <Container>
@@ -19,7 +41,74 @@ export default function PokemonComponent(props: Props) {
         <Col>
           <Image src={pokemon.mainImage} thumbnail />
         </Col>
-        <Col>Pokémon Properties</Col>
+        <Col>
+          <Row className="m-2">
+            <Row>
+              <Col xs={3}>Speed:</Col>
+              <Col>
+                <ProgressBar
+                  variant="info"
+                  now={pokemon.speed}
+                  label={pokemon.speed}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={3}>Health points: </Col>
+              <Col>
+                <ProgressBar
+                  variant="danger"
+                  now={pokemon.healthPoints}
+                  label={pokemon.healthPoints}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={3}>Attack: </Col>
+              <Col>
+                <ProgressBar
+                  variant="warning"
+                  now={pokemon.attack}
+                  label={pokemon.attack}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={3}>Defense: </Col>
+              <Col>
+                <ProgressBar
+                  variant="success"
+                  now={pokemon.defense}
+                  label={pokemon.defense}
+                />
+              </Col>
+            </Row>
+          </Row>
+          <Row className="m-2">
+            <Card className="p-0">
+              <Card.Header>Pokemon type</Card.Header>
+              <ListGroup variant="flush">
+                {pokemon.pokemonType.map((type) => (
+                  <ListGroup.Item key={pokemon.pokemonNumber}>
+                    {type}
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </Card>
+          </Row>
+          <Row className="m-2">
+            <Card className="p-0">
+              <Card.Header>Evaluation family</Card.Header>
+              <ListGroup variant="flush">
+                {pokemon.evolutionFamily.map((evolution) => (
+                  <ListGroup.Item key={pokemon.pokemonNumber}>
+                    {evolution} {getFamilyTag(pokemon, evolution)}
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </Card>
+          </Row>
+        </Col>
       </Row>
     </Container>
   );
